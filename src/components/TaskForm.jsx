2baafe3df8e29411fd5c69bcb712854017556680
Rodolfo Sanchez-Garcia <ing.rodolfosanchezg@hotmail.com>
2025-04-24 11:22:39 -0500

    Primera version del proyecto

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,48 @@
+import { useState, useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+function TaskForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const { createTask } = useContext(TaskContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = {
+      title,
+    };
+    console.log(title, description);
+    createTask({
+      title,
+      description,
+    });
+    setTitle("");
+    setDescription("");
+  };
+
+  return (
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4 ">
+        <h1 className="text-2xl font-bold text-white mb-3">Crea Tu Tarea</h1>
+        <input
+          placeholder="Escribe tu tarea"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          className="bg-slate-300 p-3 w-full mb-2"
+          autoFocus
+        />
+        <textarea
+          placeholder="Escribe la descripcion de la tarea"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+          className="bg-slate-300 p-3 w-full mb-2"
+        />
+        <button className="bg-indigo-500 px-3 py-1 text-white rounded-md">
+          Guardar
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskForm;
---
+++ src/data/task.js
@@ -0,0 +1,32 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primer Tarea",
+    description: "Mi primer Tarea",
+  },
+  {
+    id: 1,
+    title: "Mi segunda Tarea",
+    description: "Mi segunda Tarea",
+  },
+  {
+    id: 2,
+    title: "Mi tercera Tarea",
+    description: "Mi tercera Tarea",
+  },
+  {
+    id: 3,
+    title: "Mi cuarta Tarea",
+    description: "Mi cuarta Tarea",
+  },
+  {
+    id: 4,
+    title: "Mi quinta Tarea",
+    description: "Mi quinta Tarea",
+  },
+  {
+    id: 5,
+    title: "Mi sexta Tarea",
+    description: "Mi sexta Tarea",
+  },
+];
